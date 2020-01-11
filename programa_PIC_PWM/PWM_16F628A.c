144085c14b85159d8f4931fdf83df4e07b45f097
Nicolás Useche <nico311021@gmail.com>
2020-01-11 00:24:46 -0500

    Implementación y diseño 3D

--- programa_PIC_PWM/PWM_16F628A.c
+++ programa_PIC_PWM/PWM_16F628A.c
@@ -8,22 +8,22 @@
 #FUSES MCLR                     //Master Clear pin enabled
 #FUSES NOLVP                    //No low voltage prgming, B3(PIC16) or B5(PIC18) used for I/O
 #FUSES NOCPD                    //No EE protection
-
-//-------------------- PRUEBA 1 -----------------------
+//-----------------------------------------------------------------------------
 #DEFINE SW1 PORTA,0
 #DEFINE SW2 PORTA,1
 #DEFINE LED PORTB,7
 #BYTE PORTB= 6
 #BYTE PORTA= 5 
 
+
 Byte CONST display[10]= {0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x67};
 
-float duty=0.10;
+float duty=0.90;
 int onTime;
 
 void main(){   
    set_tris_a(0xFF); //TODOS ENTRADAS
-   set_tris_b(0x00); // TODOS PINES SALIDAS DIGITALES
+   set_tris_b(0x00); //TODOS PINES SALIDAS DIGITALES
    
    setup_comparator(NC_NC_NC_NC);
    setup_vref(FALSE);
@@ -31,17 +31,17 @@ void main(){
    portb = display[2]; 
    BIT_SET(LED);
    delay_ms(500);   
-   BIT_CLEAR(LED);
+   BIT_CLEAR(LED);   
    delay_ms(500);
    portb = display[1]; 
-   BIT_SET(LED);
+   BIT_SET(LED);   
    delay_ms(500);   
-   BIT_CLEAR(LED);
+   BIT_CLEAR(LED);   
    delay_ms(500);
    portb = display[0]; 
    BIT_SET(LED);   
    delay_ms(500);
-   BIT_CLEAR(LED);
+   BIT_CLEAR(LED);   
    delay_ms(500);
    while(true){   
       onTime = (int)(duty*50);    
@@ -56,31 +56,31 @@ void main(){
             duty-=0.01;            
          }
          if(duty>=0.09 && duty<=0.10){
-            portb = display[1];
+            portb = display[9];
          }
          if(duty>=0.19 && duty<=0.20){
-            portb = display[2];
+            portb = display[8];
          }
          if(duty>=0.29 && duty<=0.30){
-            portb = display[3];
+            portb = display[7];
          }
          if(duty>=0.39 && duty<=0.40){
-            portb = display[4];
+            portb = display[6];
          }
          if(duty>=0.49 && duty<=0.50){
             portb = display[5];
          }
          if(duty>=0.59 && duty<=0.60){
-            portb = display[6];
+            portb = display[4];
          }
          if(duty>=0.69 && duty<=0.70){
-            portb = display[7];
+            portb = display[3];
          }
          if(duty>=0.79 && duty<=0.80){
-            portb = display[8];
+            portb = display[2];
          }
          if(duty>=0.89 && duty<=0.90){
-            portb = display[9];
+            portb = display[1];
          }          
    }
 }
